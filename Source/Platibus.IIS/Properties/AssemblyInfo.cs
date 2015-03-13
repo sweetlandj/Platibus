84ff2a0463404819e6a2412ff7669d95e7da8489
Jesse Sweetland <sweetlandj@gmail.com>
2015-03-13 17:13:40 -0500

    Found missed file rename.  Reset version back to 1.0.0.0 for re-release with new name.

--- Source/Platibus.IIS/Properties/AssemblyInfo.cs
+++ Source/Platibus.IIS/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Platibus.IIS")]
 [assembly: AssemblyDescription("IIS hosting support for the Platibus decentralized asynchronous messaging framework")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
