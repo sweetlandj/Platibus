5823aadbb4b8303cac69decdc9d4d6206267ca76
Jesse Sweetland <sweetlandj@gmail.com>
2015-03-28 08:40:10 -0500

    Added extension method for wiring up message acknowledgement to completion of the current transaction.  Closes #9

---
+++ Source/Platibus.UnitTests/MessageHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Platibus.UnitTests
+{
+    class MessageHandlerTests
+    {
+        [Test]
+        public async Task When_Enlisted_Completed_Transaction_Then_Acknowledged()
+        {
+            var message = "Hello, world!";
+            var mockMessageContext = new Mock<IMessageContext>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            var messageHandler = new DelegateMessageHandler((msg, msgCtx) =>
+            {
+                using (var scope = new TransactionScope())
+                {
+                    msgCtx.EnlistInCurrentTransaction();
+                    scope.Complete();
+                }
+            });
+
+            await messageHandler.HandleMessage(message, mockMessageContext.Object, cancellationTokenSource.Token);
+            mockMessageContext.Verify(msgCtx => msgCtx.Acknowledge(), Times.Once());
+        }
+
+        [Test]
+        public async Task When_Enlisted_Incomplete_Transaction_Then_Not_Acknowledged()
+        {
+            var message = "Hello, world!";
+            var mockMessageContext = new Mock<IMessageContext>();
+            var cancellationTokenSource = new CancellationTokenSource();
+            var messageHandler = new DelegateMessageHandler((msg, msgCtx) =>
+            {
+                using (var scope = new TransactionScope())
+                {
+                    msgCtx.EnlistInCurrentTransaction();
+                }
+            });
+
+            await messageHandler.HandleMessage(message, mockMessageContext.Object, cancellationTokenSource.Token);
+            mockMessageContext.Verify(msgCtx => msgCtx.Acknowledge(), Times.Never());
+        }
+    }
+}
---
+++ Source/Platibus/MessageContextExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Platibus
+{
+    public static class MessageContextExtensions
+    {
+        public static TransactionScope StartTransaction(this IMessageContext messageContext, TransactionScopeOption option = TransactionScopeOption.Required)
+        {
+            var scope = new TransactionScope(option);
+            messageContext.EnlistInCurrentTransaction();
+            return scope;
+        }
+
+        public static void EnlistInCurrentTransaction(this IMessageContext messageContext)
+        {
+            var currentTransaction = Transaction.Current;
+            if (currentTransaction != null)
+            {
+                currentTransaction.TransactionCompleted += (sender, e) =>
+                {
+                    var transactionStatus = e.Transaction.TransactionInformation.Status;
+                    if (transactionStatus == TransactionStatus.Committed)
+                    {
+                        messageContext.Acknowledge();
+                    }
+                };
+            }
+        }
+    }
+}
