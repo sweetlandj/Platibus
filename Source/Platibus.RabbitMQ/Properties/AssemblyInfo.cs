fd73f232895514ee00a28f232b853c0a0c8cb588
Jesse Sweetland <sweetlandj@gmail.com>
2015-08-04 11:00:23 -0500

    Version bump

--- Source/Platibus.RabbitMQ/Properties/AssemblyInfo.cs
+++ Source/Platibus.RabbitMQ/Properties/AssemblyInfo.cs
@@ -29,6 +29,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Platibus.RabbitMQ")]
 [assembly: AssemblyDescription("Platibus.RabbitMQ")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -39,17 +40,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("76c4437e-154b-4de1-ad28-c6fa8312d0fd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Source/Platibus.SQLite/Properties/AssemblyInfo.cs
+++ Source/Platibus.SQLite/Properties/AssemblyInfo.cs
@@ -30,6 +30,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Platibus.SQLite")]
 [assembly: AssemblyDescription("Platibus.SQLite")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -41,19 +42,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("67a4d81a-596f-4d1c-9c14-f2220c412975")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly:
     InternalsVisibleTo(
         "Platibus.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100716b0ee05dc996bce4a965ac87d75aa4b633877f3627b49ce4a647c5b6ae446054c21f09aa10b2fe22074cc0e5ca6425103e205e152883b3605a9cc1a85dab5aed97856f36a4de069461a14bbdfffe4f3197e0eec47721f310551392e7ea3355da27c8f8d66d296c4b929c992cee32d3a1e31ba218e325808d3ac45fbfc765d4"
