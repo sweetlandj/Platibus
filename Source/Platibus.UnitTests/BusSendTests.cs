d7e4b2567b3d6432c3bdd34181fdd9096e88d348
Jesse Sweetland <sweetlandj@gmail.com>
2017-10-10 13:47:02 -0500

    Unit tests to verify default send options

--- Source/Platibus.UnitTests/BusSendTests.cs
+++ Source/Platibus.UnitTests/BusSendTests.cs
@@ -31,8 +31,13 @@ using Platibus.Security;
 namespace Platibus.UnitTests
 {
     [Trait("Category", "UnitTests")]
-    internal class BusSendTests
+    public class BusSendTests
     {
+        protected PlatibusConfiguration Configuration = new PlatibusConfiguration
+        {
+            DefaultContentType = "text/plain"
+        };
+
         protected readonly EndpointName DefaultEndpointName = "local";
         protected readonly Uri DefaultEndpointUri = new Uri("http://localhost/default/");
         protected readonly IEndpointCredentials DefaultEndpointCredentials = new BasicAuthCredentials("default", "defaultpw");
@@ -126,6 +131,39 @@ namespace Platibus.UnitTests
         }
 
         [Fact]
+        public async Task MessagesCanExpire()
+        {
+            var mockTransportService = GivenMockTransportService();
+            const string messageContent = "Hello, world!";
+            using (var bus = await GivenConfiguredBus(mockTransportService.Object))
+            {
+                var options = new SendOptions
+                {
+                    TTL = TimeSpan.FromMinutes(15)
+                };
+                await bus.Send(messageContent, options);
+            }
+
+            // Only one call to ITransportService.SendMessage
+            mockTransportService.Verify(
+                ts => ts.SendMessage(
+                    It.IsAny<Message>(),
+                    It.IsAny<IEndpointCredentials>(),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            // Specifically, only one call to ITransportService.SendMessage for this message
+            // addressed to the expected destination
+            mockTransportService.Verify(
+                ts => ts.SendMessage(
+                    It.Is<Message>(m => m.Content == messageContent &&
+                                        m.Headers.Expires <= DateTime.UtcNow.AddMinutes(15)),
+                    DefaultEndpointCredentials,
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+        }
+
+        [Fact]
         public async Task MessageCanBeAddressedToSpecificEndpointUriWithOverridingCredentials()
         {
             var overridingCredentials = new BasicAuthCredentials("override", "overridepw");
@@ -152,8 +190,54 @@ namespace Platibus.UnitTests
             // addressed to the expected destination with the specified override credentials
             mockTransportService.Verify(
                 ts => ts.SendMessage(
-                    It.Is<Message>(m => m.Content == messageContent && m.Headers.Destination == OtherEndpointUri),
+                    It.Is<Message>(m => m.Content == messageContent &&
+                                        m.Headers.Destination == OtherEndpointUri),
                     overridingCredentials,
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+        }
+
+        [Fact]
+        public async Task DefaultSendOptionsUsedWhenNoExplicitSendOptions()
+        {
+            var defaultSendOptionCredentials = new BasicAuthCredentials("default", "defaultpw");
+            Configuration.DefaultSendOptions = new SendOptions
+            {
+                TTL = TimeSpan.FromMinutes(15),
+                Synchronous = true,
+                ContentType = "application/json",
+                Credentials = defaultSendOptionCredentials
+            };
+
+            var mockTransportService = GivenMockTransportService();
+            var content = new
+            {
+                message = "Hello, World!"
+            };
+
+            const string expectedMessageContent = "{\"message\":\"Hello, World!\"}";
+
+            using (var bus = await GivenConfiguredBus(mockTransportService.Object))
+            {
+                await bus.Send(content);
+            }
+
+            // Only one call to ITransportService.SendMessage
+            mockTransportService.Verify(
+                ts => ts.SendMessage(
+                    It.IsAny<Message>(),
+                    It.IsAny<IEndpointCredentials>(),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            // Specifically, only one call to ITransportService.SendMessage for this message
+            // with header values based on the default credentials
+            mockTransportService.Verify(
+                ts => ts.SendMessage(
+                    It.Is<Message>(m => m.Content == expectedMessageContent && 
+                    m.Headers.Synchronous &&
+                    m.Headers.Expires <= DateTime.UtcNow.AddMinutes(15)),
+                    defaultSendOptionCredentials,
                     It.IsAny<CancellationToken>()),
                 Times.Once());
         }
@@ -175,20 +259,15 @@ namespace Platibus.UnitTests
             var allMessages = new MessageNamePatternSpecification(".*");
             var noMatchSpecification = new Mock<IMessageSpecification>().Object;
             var messageQueueingService = new Mock<IMessageQueueingService>();
+            
+            Configuration.AddEndpoint(DefaultEndpointName, new Endpoint(DefaultEndpointUri, DefaultEndpointCredentials));
+            Configuration.AddSendRule(new SendRule(allMessages, DefaultEndpointName));
 
-            var config = new PlatibusConfiguration
-            {
-                DefaultContentType = "text/plain"
-            };
-
-            config.AddEndpoint(DefaultEndpointName, new Endpoint(DefaultEndpointUri, DefaultEndpointCredentials));
-            config.AddSendRule(new SendRule(allMessages, DefaultEndpointName));
-
-            config.AddEndpoint(OtherEndpointName, new Endpoint(OtherEndpointUri, OtherEndpointCredentials));
-            config.AddSendRule(new SendRule(noMatchSpecification, OtherEndpointName));
+            Configuration.AddEndpoint(OtherEndpointName, new Endpoint(OtherEndpointUri, OtherEndpointCredentials));
+            Configuration.AddSendRule(new SendRule(noMatchSpecification, OtherEndpointName));
 
             var baseUri = new Uri("http://localhost/platibus");
-            var bus = new Bus(config, baseUri, transportService, messageQueueingService.Object);
+            var bus = new Bus(Configuration, baseUri, transportService, messageQueueingService.Object);
             await bus.Init();
             return bus;
         }
